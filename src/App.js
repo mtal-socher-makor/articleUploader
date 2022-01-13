188eb4392ac62fb2b3446bbbdfc6ab688b7dff06
unknown <ochayoun@makor-capital.com>
2022-01-13 11:32:40 +0200

    initial login page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import UploadForm from './Components/UploadForm';
-import { setAuthToken } from './utils/constants';
-import { useEffect } from 'react';
-import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import DateFnsUtils from '@date-io/date-fns';
+import UploadForm from './Components/UploadForm'
+import { setAuthToken } from './utils/constants'
+import { useEffect } from 'react'
+import { MuiPickersUtilsProvider } from '@material-ui/pickers'
+import DateFnsUtils from '@date-io/date-fns'
+import Login from './Components/Login'
 
 function App() {
   useEffect(() => {
-    setAuthToken('123');
-  }, []);
+    setAuthToken('123')
+  }, [])
 
   return (
-    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <UploadForm />
-    </MuiPickersUtilsProvider>
-  );
+    <Login></Login>
+    // <MuiPickersUtilsProvider utils={DateFnsUtils}>
+    //     <UploadForm />
+    // </MuiPickersUtilsProvider>
+  )
 }
 
-export default App;
+export default App
